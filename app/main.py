37be6df84be65d07667cc96f2f861e05242e957b
tonybotros1 <tonybotros6@gmail.com>
2025-09-01 20:55:38 +0300

    fixes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from .routes import cars, jobs, brands
+from .routes import  brands
 
 app = FastAPI(title="DataHub AI")
 
 # Routers
-app.include_router(cars.router, prefix="/cars", tags=["Cars"])
-app.include_router(jobs.router, prefix="/jobs", tags=["Job Cards"])
+# app.include_router(cars.router, prefix="/cars", tags=["Cars"])
+# app.include_router(jobs.router, prefix="/jobs", tags=["Job Cards"])
 app.include_router(brands.router, prefix="/brands", tags=["Brands"])
 
 @app.get("/")
